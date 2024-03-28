LineageOS/android_hardware_lineage_interfaces

usb: 1.3-basic: Allow devices to customize usb control {path,enable,disable}
[cafebabe] Port to USB HAL

Change-Id: I0bf9b8918b11a9ef6ce85c56ca85377b2031b114

usb/1.3-basic/Usb.cpp
@@ -133,8 +133,8 @@ Return<void> Usb::enableContaminantPresenceProtection(const hidl_string& portNam
 
 // Methods from ::android::hardware::usb::V1_3::IUsb follow.
 Return<bool> Usb::enableUsbDataSignal(bool enable) {
-    bool success =
-            android::base::WriteStringToFile(enable ? "0" : "1", "/proc/sys/kernel/deny_new_usb");
+    bool success = android::base::WriteStringToFile(
+            enable ? USB_CONTROL_ENABLE : USB_CONTROL_DISABLE, USB_CONTROL_PATH);
     if (success)
         queryPortStatus();
     else

Project-PixelStar/hardware_lineage_interfaces

usb/1.3-basic/Usb.cpp
@@ -133,8 +133,8 @@ Return<void> Usb::enableContaminantPresenceProtection(const hidl_string& portNam
 
 // Methods from ::android::hardware::usb::V1_3::IUsb follow.
 Return<bool> Usb::enableUsbDataSignal(bool enable) {
-    bool success =
-            android::base::WriteStringToFile(enable ? "0" : "1", "/proc/sys/kernel/deny_new_usb");
+    bool success = android::base::WriteStringToFile(
+            enable ? USB_CONTROL_DISABLE : USB_CONTROL_ENABLE, USB_CONTROL_PATH);
     if (success)
         queryPortStatus();
     else